1420c4552307cc7d907339099f0146779f029113
linchikinLG <learninglinchikin@gmail.com>
2024-07-23 19:06:16 +0000

    modified:   RecycleBin/recycle.sh 	new file:   RecycleBin/restore.sh

--- RecycleBin/recycle.sh
+++ RecycleBin/recycle.sh
@@ -1,40 +1,32 @@
 declare arg0="$(basename $0)";
-declare arg1="$1";
-declare RECYCLEBIN_PATH="${HOME}/recyclebin";
+declare RECYCLEBIN="${HOME}/recyclebin";
 declare RESTORE_INFO="${HOME}/.restore.info";
 
-function recycle(){
-	if [ -z $1 ]; then
-		echo "No filename provided";
-		exit 1;
-	fi
-	if [ -d $1 ]; then
-		echo "Directory detected. Not a valid file.";
-		exit 1;
-	fi
-	if [ ! -e $1 ]; then
-		echo "File does not exist";
-		exit 1;
-	else
-		local iNode=$(ls -i $1 | cut -d " " -f 1);
-		local fullPath=$(readlink -f $1);
-		local fileName=$(basename $fullPath);
-		local restoreFileName="${fileName}_${iNode}";
-		if [ $fullPath = "$(pwd)/${arg0}" ]; then
-			echo "Forbidden to delete recycle";
-			exit 1;
-		fi
-		touch "${RESTORE_INFO}";
-		echo "${restoreFileName}:${fullPath}" >> $RESTORE_INFO;
-		mv $1 "${RECYCLEBIN_PATH}/${restoreFileName}";
-	fi
-}
-
-if [ -e $RECYCLEBIN_PATH ]; then
-	recycle "$arg1";
-else
-	mkdir $RECYCLEBIN_PATH;
-	echo "recyclebin is created for you now";
-	recycle "$arg1";
+if [ ! -e $RECYCLEBIN ]; then
+	mkdir $RECYCLEBIN;
+        echo "recyclebin is created for you now";
 fi
-
+if [ -z $1 ]; then
+	echo "No filename provided";
+	exit 1;
+fi
+if [ -d $1 ]; then
+	echo "Directory detected. Not a valid file.";
+	exit 1;
+fi
+if [ ! -e $1 ]; then
+	echo "File does not exist";
+	exit 1;
+else
+	declare iNode=$(ls -i $1 | cut -d " " -f 1);
+	declare fullPath=$(readlink -f $1);
+	declare fileName=$(basename $fullPath);
+	declare restoreFileName="${fileName}_${iNode}";
+	if [ $fullPath = "$(pwd)/${arg0}" ]; then
+		echo "Forbidden to delete recycle";
+		exit 1;
+	fi
+	touch "${RESTORE_INFO}";
+	echo "${restoreFileName}:${fullPath}" >> $RESTORE_INFO;
+	mv $1 "${RECYCLEBIN}/${restoreFileName}";
+fi
---
+++ RecycleBin/restore.sh
@@ -0,0 +1,35 @@
+declare arg0="$(basename $0)";
+declare RECYCLEBIN="${HOME}/recyclebin";
+declare RESTORE_INFO="${HOME}/.restore.info";
+
+function restore(){
+	if [ -f $2 ]; then
+		echo -n "Do you want to overwrite? (Y/N)";
+		read input;
+		if [[ ! "$input" =~ ^[y|Y].*  ]]; then
+			echo "Cancelled...";
+			exit 0;
+		fi
+	fi
+	mv "${RECYCLEBIN}/$1" "$2";
+	sed -i "/^$1/d" $RESTORE_INFO;
+}
+
+if [ ! -e $RECYCLEBIN ]; then
+        mkdir $RECYCLEBIN;
+        echo "recyclebin is created for you now";
+fi
+if [ -z $1 ]; then
+        echo "No filename provided";
+        exit 1;
+fi
+for i in $(cat $RESTORE_INFO); do
+	declare file=$(echo $i | cut -d ":" -f 1);
+	declare originalFile=$(echo $i | cut -d ":" -f 2);
+	if [ $file = $1 ]; then
+		restore $file $originalFile;
+		exit 1;
+	fi
+done
+echo "File does not exist";
+exit 1;
